7643f4189d19465302071f7c87c4b89ced853864
Serhii <mel.gipppson@gmail.com>
2022-10-23 16:26:05 +0300

    added images folder and logic into Categories component

--- src/components/Header/Categories/Categories.jsx
+++ src/components/Header/Categories/Categories.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const categories = ['Все', 
+                          'Мясные',
+                          'Вегетарианская',
+                          'Гриль', 
+                          'Острые', 
+                          'Закрытые'];
+  const categoriesList = categories.map((el, index) => {
+    return <li key={index}
+              className={activeIndex === index ? 'active' : ''}
+              onMouseEnter={(e) => onSetActiveClass(index)}
+              onMouseLeave={(e) => onRemoveActiveClass(index)}>
+                {el}
+            </li>
+  })
+  
+  const onSetActiveClass = (index) => {
+    setActiveIndex(index);
+  }
+
+  const onRemoveActiveClass = (e) => {
+    setActiveIndex(null);
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categoriesList}
       </ul>
     </div>
   );
