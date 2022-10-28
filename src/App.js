e80aa3513d5d05d6f3e03395f771fbfaae84febe
Serhii <mel.gipppson@gmail.com>
2022-10-28 12:14:33 +0300

    added skeletons without loading pizza-blocks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { Categories } from './components/Header/Categories/Categories';
-import { Header } from './components/Header/Header/Header';
-import { PizzaBlock } from './components/Header/Pizza-block/Pizza-block';
-import { Sort } from './components/Header/Sort/Sort';
+import { Categories } from './components/Categories/Categories';
+import { Header } from './components/Header/Header';
+import { PizzaBlock } from './components/Pizza-block/Pizza-block';
+import { Sort } from './components/Sort/Sort';
 import './scss/app.scss';
-import pizzas from './pizzas.json';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { SkeletonPizza } from './components/Pizza-block/SkeletonPizza';
 
 
 function App() {
+    const [pizzas, setPizzas] = useState([]); 
+    const [isLoading, setIsLoading] =useState(true);
+
+    const res = () => {
+        axios.get('https://6357011f9243cf412f91c477.mockapi.io/pizzaItems')
+        .then(res => {
+            setPizzas(res.data);
+            setIsLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        res();
+    }, [])
+    
+
     return (
         <div className="wrapper">
             <Header/>
@@ -18,9 +36,11 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((el, index) => {
-                            return <PizzaBlock key={index} {...el}/>
-                        })}
+                        {isLoading 
+                            ? [...new Array(6)].map((_, index) => <SkeletonPizza key={index}/>)
+                            :pizzas.map((el, index) => {
+                                return <PizzaBlock key={index} {...el}/>
+                            })}
                     </div>
                 </div>
             </div>
